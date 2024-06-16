dbdf3f259d1eab329c804426fb7a249f7664db9a
ahmed007dr <ahmed007eg@gmail.com>
2024-06-16 23:35:00 +0300

    edit spling model

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Account , MyAccountManger
+from .models import Account 
 
 admin.site.register(Account)
--- accounts/models.py
+++ accounts/models.py
@@ -1,18 +1,18 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser , BaseUserManager
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+
 # Create your models here.
 
-# customer user admin
-class MyAccountManger(BaseUserManager):
-    def create_user(self,first_name,last_name,username,email,password=None):
+class MyAccountManager(BaseUserManager):
+    def create_user(self, first_name, last_name, email, password=None):
         if not email:
             raise ValueError('User must have an email address')
-        if not username:
-            raise ValueError('User must have an username')
+        if not email:
+            raise ValueError('User must have an email address')
         
         user = self.model(
             email = self.normalize_email(email),
-            username = username,
+            username = '',
             first_name = first_name,
             last_name = last_name,
         )
@@ -20,22 +20,19 @@ class MyAccountManger(BaseUserManager):
         user.save(using=self._db)
         return user
     
-    def create_superuser(self,firs_name,last_name,email,username,password):
+    def create_superuser(self, first_name, last_name, email, password):
         user = self.create_user(
             email = self.normalize_email(email),
-            username = username,
-            first_name = firs_name,
+            username = '',
+            first_name = first_name,
             last_name = last_name,
             password = password,
-            )
+        )
         user.is_admin = True
         user.is_active = True
         user.is_staff = True
-        user.is_superadmin = True
         user.save(using=self._db)
         return user
-
-
 
 class Account(AbstractBaseUser):
     first_name = models.CharField(max_length=50)
@@ -45,23 +42,19 @@ class Account(AbstractBaseUser):
     phone_number = models.CharField(max_length=50)
 
     #required
-    date_joined = models.DateField(auto_now_add=True)
-    last_login = models.DateField(auto_now_add=True)
-    is_admin = models.BooleanField(default=False)
-    is_active = models.BooleanField(default=False)
-    is_staff = models.BooleanField(default=False)
-    is_superuser = models.BooleanField(default=False)
+    date_joined = models.DateTimeField(auto_now_add=True)
+    last_login = models.DateTimeField(auto_now_add=True)
 
-    USERNAME_FEILD = 'email'
-    REQUIRED_FIELDS = ['username','first_name', 'last_name', 'phone_number']
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username', 'first_name', 'last_name', 'phone_number']
     
-    objects = MyAccountManger()
+    objects = MyAccountManager()
 
     def __str__(self):
         return self.email
 
-    def has_perm (self , perms, obj_None):
+    def has_perm(self, perm, obj=None):
         return self.is_admin
-    
-    def has_module_perms(self,add_label):
+
+    def has_module_perms(self, app_label):
         return True
