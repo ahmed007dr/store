d3d701e571626a61d03cc3bbb7a57464caed51f4
ahmed007dr <ahmed007eg@gmail.com>
2024-06-17 13:30:28 +0300

    add product count

--- store/views.py
+++ store/views.py
@@ -4,9 +4,11 @@ from .models import Product
 
 def store(request):
     products = Product.objects.all().filter(is_available=True)
+    products_count = products.count()
 
     context = {
         'products': products,
+        'products_count':products_count,
     }
 
     return render(request,'store/store.html',context)
