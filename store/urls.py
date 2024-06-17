11cef390f31f55eb35086dbcd38445f494e2d916
ahmed007dr <ahmed007eg@gmail.com>
2024-06-17 13:42:43 +0300

    categories views

--- store/urls.py
+++ store/urls.py
@@ -4,4 +4,8 @@ from . import views
 
 urlpatterns = [
     path('',views.store,name='store'),
+    path('<slug:category_slug>/',views.store,name='products_by_category'),
+    # path('',views.store,name='store'),
+    # path('',views.store,name='store'),
+
 ]
