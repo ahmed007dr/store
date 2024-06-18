c54afcca9156c53196fb656d4c9ce7c8c909b867
ahmed007dr <ahmed007eg@gmail.com>
2024-06-19 01:29:57 +0300

    create calculate tax and total

--- carts/views.py
+++ carts/views.py
@@ -48,6 +48,10 @@ def cart(request):
         for cart_item in cart_items:
             total += (cart_item.product.price * cart_item.quantity)
             quantity += cart_item.quantity
+
+        tax = (14 *total)/100
+        grand_total = total + tax
+
         
     except ObjectDoesNotExist:
         cart = None
@@ -60,6 +64,8 @@ def cart(request):
         'cart_items': cart_items,
         'total': total,
         'quantity': quantity,
+        'tax':tax,
+        'grand_total':grand_total,
     }
     
     return render(request, 'store/carts.html', context)
