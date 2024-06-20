83ca9e7aa4d51c077647895739a4696b3dd74459
ahmed007dr <ahmed007eg@gmail.com>
2024-06-21 01:04:50 +0300

    display variations (colors and sizes)

--- carts/views.py
+++ carts/views.py
@@ -13,8 +13,10 @@ def _cart_id(request):
 
 
 def add_cart(request,product_id):
-    color = request.GET['color']
-    size = request.GET['size']
+    if request.method == 'POST':
+            
+        color = request.POST['color']
+        size = request.POST['size']
     
 
     product = Product.objects.get(id=product_id) # get the product 
