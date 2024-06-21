b0490bfe14fb91844dfd0469dc1d63be78a7918b
ahmed007dr <ahmed007eg@gmail.com>
2024-06-21 04:09:52 +0300

    cart one item alot of option color and size p1

--- carts/views.py
+++ carts/views.py
@@ -1,9 +1,8 @@
-from django.shortcuts import render ,redirect 
-from store.models import Product
+from django.shortcuts import get_object_or_404, redirect ,render
+from store.models import Product,Variation
 from .models import Cart ,CartItem
 from django.core.exceptions import ObjectDoesNotExist
 from django.http import HttpResponse
-from store.models import Variation
 
 def _cart_id(request):
     cart = request.session.session_key
@@ -12,54 +11,49 @@ def _cart_id(request):
         cart = request.session.session_key  # Update cart after session creation
     return cart
 
-
-def add_cart(request,product_id):
-    product = Product.objects.get(id=product_id) # get the product 
-    product_varition = []
+def add_cart(request, product_id):
+    product = get_object_or_404(Product, id=product_id)
+    product_variations = []
+    
     if request.method == 'POST':
         for item in request.POST:
             key = item
             value = request.POST[key]
             
             try:
-                varition = Variation.objects.get(product=product , variation_category__iexact=key,variation_value__iexact=value)
-                product_varition.append(varition)
-            except:
+                variation = Variation.objects.get(product=product, 
+                                                  variation_category__iexact=key,
+                                                  variation_value__iexact=value)
+                product_variations.append(variation)
+            except Variation.DoesNotExist:
                 pass
-
-
-
+    
     try:
-        
-        cart = Cart.objects.get(cart_id=_cart_id(request)) # get the cart using the cart_id present in the session
+        cart = Cart.objects.get(cart_id=_cart_id(request))
     except Cart.DoesNotExist:
-        cart = Cart.objects.create(
-            cart_id = _cart_id(request)
-        )
-    cart.save()
+        cart = Cart.objects.create(cart_id=_cart_id(request))
+    
+    is_cart_item_exists = CartItem.objects.filter(product=product, cart=cart).exists()
 
-    try :
-        cart_item = CartItem.objects.get(product=product , cart=cart)
-        if len(product_varition) > 0:
-            cart_item.variations.clear()
-            for item in product_varition:
-                cart_item.variations.add(item)
-
-        cart_item.quantity += 1 # if cart item already exists then increment the quantity
-        cart_item.save()
-    except CartItem.DoesNotExist:
-        cart_item = CartItem.objects.create(
-            product = product,
-            quantity = 1 ,
-            cart = cart , 
-        )
-        if len(product_varition) > 0:
-            cart_item.variations.clear()
-
-            for item in product_varition:
-                cart_item.variations.add(item)
-
-        cart_item.save()
+    if is_cart_item_exists:
+        cart_items = CartItem.objects.filter(product=product, cart=cart)
+        
+        for cart_item in cart_items:
+            existing_variations = list(cart_item.variations.all())
+            
+            if existing_variations == product_variations:
+                cart_item.quantity += 1
+                cart_item.save()
+                return redirect('cart')
+        
+        new_cart_item = CartItem.objects.create(product=product, cart=cart, quantity=1)
+        new_cart_item.variations.set(product_variations)
+        new_cart_item.save()
+    else:
+        new_cart_item = CartItem.objects.create(product=product, cart=cart, quantity=1)
+        new_cart_item.variations.set(product_variations)
+        new_cart_item.save()
+    
     return redirect('cart')
 
 
