ba6291ed41e1928f27be5a15daf660e786bb9927
ahmed007dr <ahmed007eg@gmail.com>
2024-06-21 03:10:07 +0300

    varation

--- carts/views.py
+++ carts/views.py
@@ -3,7 +3,7 @@ from store.models import Product
 from .models import Cart ,CartItem
 from django.core.exceptions import ObjectDoesNotExist
 from django.http import HttpResponse
-
+from store.models import Variation
 
 def _cart_id(request):
     cart = request.session.session_key
@@ -14,16 +14,21 @@ def _cart_id(request):
 
 
 def add_cart(request,product_id):
+    product = Product.objects.get(id=product_id) # get the product 
+    product_varition = []
     if request.method == 'POST':
         for item in request.POST:
             key = item
             value = request.POST[key]
             
-            print(key,value)
+            try:
+                varition = Variation.objects.get(product=product , variation_category__iexact=key,variation_value__iexact=value)
+                product_varition.append(varition)
+            except:
+                pass
 
 
 
-    product = Product.objects.get(id=product_id) # get the product 
     try:
         cart = Cart.objects.get(cart_id=_cart_id(request)) # get the cart using the cart_id present in the session
     except Cart.DoesNotExist:
