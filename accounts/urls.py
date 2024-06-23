5d8e1b44ecdbf4dd4c64ad59a958149985b8a681
ahmed007dr <ahmed007eg@gmail.com>
2024-06-24 00:57:10 +0300

    url reactivae

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('login/', views.user_login, name='login'),
     path('logout/', views.user_logout, name='logout'),
     path('activate/<str:uidb64>/<str:token>/', views.activate, name='activate'),
-    # path('portfolio/', views.portfolio, name='portfolio'),
+    path('resend-verification-email/', views.resend_verification_email, name='resend_verification_email'),
     # path('blog/', views.blog, name='blog'),
     # path('blog/<slug:slug>/', views.blog_detail, name='blog_detail'),
 
