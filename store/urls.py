425d6c1232531b8ea678fed0efb6ea2a22c67fa8
ahmed007dr <ahmed007eg@gmail.com>
2024-06-25 20:19:38 +0300

    create checkout html and urls

--- store/urls.py
+++ store/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('category/<slug:category_slug>/',views.store,name='products_by_category'),
     path('category/<slug:category_slug>/<slug:product_slug>',views.product_details,name='product_details'),
     path('search/',views.search,name='search'),
+    path('checkout/', views.checkout, name='checkout'),
 
 ]
--- store/views.py
+++ store/views.py
@@ -79,3 +79,7 @@ def search(request):
     }
 
     return render(request, 'store/store.html', context)
+
+
+def checkout(request):
+    return render(request,'store/checkout.html')
