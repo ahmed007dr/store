2492eb47b4e5223857a67588b1a27ac57acba463
ahmed007dr <ahmed007eg@gmail.com>
2024-06-28 16:33:25 +0300

    fix this issue

--- carts/context_processors.py
+++ carts/context_processors.py
@@ -5,23 +5,14 @@ from .views import _cart_id
 #number of cart items
 def counter(request):
     cart_count = 0
-
     if 'admin' in request.path:
-        return {}  # Return an empty dictionary for admin views
+        return {}
     else:
         try:
-            if request.user.is_authenticated:
-                cart_items = CartItem.objects.filter(user=request.user, is_active=True)
-            else:
-                cart = Cart.objects.get(cart_id=_cart_id(request))
-                cart_items = CartItem.objects.filter(cart=cart, is_active=True)
-
+            cart = Cart.objects.filter(cart_id = _cart_id(request))
+            cart_items = CartItem.objects.all().filter(cart=cart[:1])
             for item in cart_items:
                 cart_count += item.quantity
-
         except Cart.DoesNotExist:
             cart_count = 0
-        except CartItem.DoesNotExist:
-            cart_count = 0
-
-    return {'cart_count': cart_count}
+    return dict(cart_count = cart_count)
