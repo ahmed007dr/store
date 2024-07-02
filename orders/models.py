75fb0d864a79d45ee61831d35306d1292d9d20b7
ahmed007dr <ahmed007eg@gmail.com>
2024-07-02 22:39:39 +0300

    add more table for app orders

--- orders/models.py
+++ orders/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from accounts.models import Account
+from store.models import Product,Variation
 # Create your models here.
 
 
@@ -44,3 +45,23 @@ class Order(models.Model):
     is_ordered = models.BooleanField(default=False)
     created_at = models.DateField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.first_name
+    
+class OrderProduct(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    payment = models.ForeignKey(Payment, on_delete=models.CASCADE)
+    user = models.ForeignKey(Account, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    variation = models.ForeignKey(Variation,on_delete=models.CASCADE)
+    color = models.CharField(max_length=100)
+    size = models.CharField(max_length=100)
+    quantity = models.IntegerField()
+    product_price = models.FloatField()
+    ordered = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.product.product_name
